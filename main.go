6bc4d1a9a6408f6eb49d96ed2eebb6009293555c
Jiten <jpalaparthi@gmail.com>
2021-09-29 15:42:58 +0530

    Add hellowrold restful service

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	http.HandleFunc("/", helloWorld)
+	http.ListenAndServe(":50060", nil)
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello World")
 }
